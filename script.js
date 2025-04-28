c09d5a4dabfed88875d765bd95fc42b8b17c1d24
YoungCoder-Hlib <glowsor450@gmail.com>
2025-04-28 11:20:59 +0300

    Json fetching, and object creating added. Planning on adding costum deals

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let objh = document.querySelector("#objholder");
+let Adder = document.querySelector("#AddObjButton");
+let AfterAdder = document.querySelector("#Add");
+let contenty = document.querySelector("#contenty");
+let panel = document.querySelector("#panel");
 //console.log(arrayOfVitaminObjects);
 let Itemy = [];
 async function getObj(item) {
@@ -11,9 +15,29 @@ async function getObj(item) {
         data.Items.forEach(item => {
             Itemy.push(item);
         });
-        for(let i=0; i<Itemy.length; i++){
+        Itemy.forEach((obj) => {
          let keyda = document.createElement("div");
-        }
+         keyda.classList.add("obj")
+         keyda.innerHTML = `
+                <div class="item-title">${obj.title}</div>
+                <div class="item-image">
+                <img src="${obj.image}">
+                </div>
+                <div class="parts-pay">
+                <div><img src="img/mona.png" alt="PLATY">${obj.monopay}</div>
+                <div><img src="img/Privat.png" alt="MENI PLATY">${obj.privatpay}</div>
+                </div>
+                <div class="price">
+                <div><span>${obj.pricewithoutdisc} </span><sup>грн</sup></div>
+                <div><span>${obj.pricewithdisc} </span><sup>грн</sup></div>
+                </div>
+                <div class="price-bonus">
+                <div>Ціна за купоном</div>
+                <div><span>${obj.pricewithcoup} </span><sup>грн</sup></div>
+                </div>
+            `;
+            document.getElementById('objholder').appendChild(keyda);
+        });
         return data;
     }
     catch (error){
@@ -23,3 +47,12 @@ async function getObj(item) {
     }
 }
 getObj('json/addplate.json');
+
+Adder.addEventListener("click", function(){
+    contenty.style.display = "none";
+    panel.style.display = "block";
+});
+AfterAdder.addEventListener("click", function(){
+    contenty.style.display = "block";
+    panel.style.display = "none";
+});
